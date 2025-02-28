0c892bc1f99bd63d9b0c8b1cfd56467f5d4cd5be
Jakub Vaclavik <jakub.vaclavik@muziker.com>
2025-02-28 16:15:23 +0100

    Add edit and update for notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,7 +4,7 @@ class NotesController < ApplicationController
     end
 
     def show
-        @note = Note.find(params[:id])
+      @note = Note.find(params[:id])
     end
 
     def new
@@ -14,5 +14,17 @@ class NotesController < ApplicationController
       @note = Note.create(name: params[:name], text: params[:text])
 
       redirect_to @note # note_url(id: @note.id) == note_url(@note.id) == note_url(@note) == @note --> All valid - Rails magic (using helper 'as: :note' in routes)
+    end
+
+    def edit
+      @note = Note.find(params[:id])
+    end
+
+    def update
+      @note = Note.find(params[:id])
+
+      @note.update(name: params[:name], text: params[:text])
+
+      redirect_to @note
     end
 end
