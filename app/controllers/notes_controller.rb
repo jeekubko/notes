d5e2b54d6bba3153b8b0b1a4829a3992b18bd12c
Jakub Vaclavik <jakub.vaclavik@muziker.com>
2025-03-07 08:24:47 +0100

    Add validation to note name

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,17 +13,24 @@ class NotesController < ApplicationController
   end
 
   def create
-    @note = Note.create(note_params)
-    redirect_to @note # note_url(id: @note.id) == note_url(@note.id) == note_url(@note) == @note --> All valid - Rails magic (using helper 'as: :note' in routes)
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to @note # note_url(id: @note.id) == note_url(@note.id) == note_url(@note) == @note --> All valid - Rails magic (using helper 'as: :note' in routes)
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
-    @note.update(note_params)
-    
-    redirect_to @note
+    if @note.update(note_params)
+      redirect_to @note
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   def destroy
